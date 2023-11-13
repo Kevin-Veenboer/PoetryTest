1e02c8ce6fbc4da86facb9b8e8fb6b5e2c41778a
Kevin-Veenboer <135600479+Kevin-Veenboer@users.noreply.github.com>
2023-11-13 13:36:20 +0100

    Created easystat package

---
+++ easystat/src/easystat/shortcuts.py
@@ -0,0 +1,7 @@
+# src/easystat/shortcuts.py
+import numpy as np
+
+
+def stdev_of_mean(values):
+    # Calculate the standard deviation of the mean
+    return np.std(values) / np.sqrt(len(values))
---
+++ easystat/tests/try_shortcuts.py
@@ -0,0 +1,4 @@
+# tests/try_shortcuts.py
+from easystat.shortcuts import stdev_of_mean
+
+print(f"{stdev_of_mean([1, 2, 2, 2, 3])=}")
